23866c9c9a2a9ae65d7d485b1af2320fac60329a
Faha Tagizade <Fakhriyya16>
2024-05-03 14:27:07 +0400

    finished navbar and first section

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,27 @@
 "use strict";
+
+let dropdown = document.querySelectorAll(".home");
+let categoryDropdown = document.querySelector(".category");
+
+dropdown.forEach(element => {
+    element.addEventListener("mouseover", function(){
+        document.querySelector(".open-down").classList.add("active-dropdown");
+    })
+    element.addEventListener("mouseout", function(){
+        document.querySelector(".open-down").classList.remove("active-dropdown");
+    })
+});
+
+dropdown.addEventListener("mouseover", function(){
+    document.querySelector(".open-down").classList.add("active-dropdown");
+})
+dropdown.addEventListener("mouseout", function(){
+    document.querySelector(".open-down").classList.remove("active-dropdown");
+})
+
+categoryDropdown.addEventListener("mouseover", function(){
+    document.querySelector(".category-dropdown").classList.add("active-category");
+})
+categoryDropdown.addEventListener("mouseout", function(){
+    document.querySelector(".category-dropdown").classList.remove("active-category");
+})

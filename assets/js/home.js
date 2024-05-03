8bcb9e0abf5fcbeb272bcdc86e7ca211ff75651e
Faha Tagizade <Fakhriyya16>
2024-05-03 21:44:08 +0400

    finished 2 sections and made navbar fixed

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,27 +1,86 @@
 "use strict";
 
-let dropdown = document.querySelectorAll(".home");
+let dropdown = document.querySelector(".home");
 let categoryDropdown = document.querySelector(".category");
+let openedList = document.querySelector(".open-down");
+let openedCategories = document.querySelector(".category-dropdown")
 
-dropdown.forEach(element => {
-    element.addEventListener("mouseover", function(){
-        document.querySelector(".open-down").classList.add("active-dropdown");
-    })
-    element.addEventListener("mouseout", function(){
-        document.querySelector(".open-down").classList.remove("active-dropdown");
-    })
+function openDropdown() {
+    openedList.classList.add("active-dropdown");
+}
+
+function closeDropdown() {
+    openedList.classList.remove("active-dropdown");
+}
+
+
+dropdown.addEventListener("mouseover", openDropdown);
+
+
+dropdown.addEventListener("mouseout", closeDropdown);
+
+
+openedList.addEventListener("mouseover", openDropdown);
+
+
+document.addEventListener("mouseover", function (event) {
+
+    if (!dropdown.contains(event.target) && !openedList.contains(event.target)) {
+        closeDropdown();
+    }
 });
 
-dropdown.addEventListener("mouseover", function(){
-    document.querySelector(".open-down").classList.add("active-dropdown");
+
+function openCategoryDropdown() {
+    openedCategories.classList.add("active-category");
+}
+
+function closeCategoryDropdown() {
+    openedCategories.classList.remove("active-category");
+}
+
+
+categoryDropdown.addEventListener("mouseover", openCategoryDropdown);
+openedCategories.addEventListener("mouseover", openCategoryDropdown);
+
+
+categoryDropdown.addEventListener("mouseout", function (event) {
+
+    if (!categoryDropdown.contains(event.relatedTarget) && !openedCategories.contains(event.relatedTarget)) {
+        closeCategoryDropdown();
+    }
+});
+
+openedCategories.addEventListener("mouseout", function (event) {
+    if (!categoryDropdown.contains(event.relatedTarget) && !openedCategories.contains(event.relatedTarget)) {
+        closeCategoryDropdown();
+    }
+});
+
+let arrow = document.querySelector(".visible");
+let disappearArrow = document.querySelector(".disappear")
+
+arrow.addEventListener("mouseover", function () {
+    disappearArrow.classList.remove("disappear");
+    disappearArrow.classList.add("visible");
+    this.classList.remove("visible");
+    this.classList.add("disappear");
+    arrow.removeEventListener("mouseover", mouseOverHandler);
 })
-dropdown.addEventListener("mouseout", function(){
-    document.querySelector(".open-down").classList.remove("active-dropdown");
+arrow.addEventListener("mouseout", function () {
+    disappearArrow.classList.add("disappear");
+    disappearArrow.classList.remove("visible");
+    this.classList.add("visible");
+    this.classList.remove("disappear");
 })
 
-categoryDropdown.addEventListener("mouseover", function(){
-    document.querySelector(".category-dropdown").classList.add("active-category");
-})
-categoryDropdown.addEventListener("mouseout", function(){
-    document.querySelector(".category-dropdown").classList.remove("active-category");
-})
+const navbar = document.querySelector(".navbar");
+let startPosition = navbar.offsetTop;
+function stickynavbar() {
+  if (window.scrollY > 90 +startPosition) {    
+    navbar.classList.add('fixed-top');
+  } else {
+    navbar.classList.remove('fixed-top');    
+  }
+}
+window.addEventListener('scroll', stickynavbar);
